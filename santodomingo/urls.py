ef1d323e9dab6004419839be56a340b32e185bd0
danielessu <danielessu@unisabana.edu.co>
2025-05-22 13:19:00 -0500

    tokens

--- santodomingo/urls.py
+++ santodomingo/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import dish_list, create_order, order_list_view, update_order_status_view
-from django.contrib.auth.views import LoginView, LogoutView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('dishes/', dish_list, name='dish_list'),
     path('orders/', create_order, name='create_order'),
-    path('staff/login/', LoginView.as_view(template_name='staff/login.html'), name='login'),
-    path('staff/logout/', LogoutView.as_view(), name='logout'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('staff/orders/', order_list_view, name='order_list'),
     path('staff/orders/<int:order_id>/update_status/', update_order_status_view, name='update_order_status'),
 ]
